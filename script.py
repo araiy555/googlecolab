d6606c6c22b4893e88fe81dd9fe5906b5f5a0a32
araiy555 <k016c1200@it-neec.jp>
2025-10-11 10:50:45 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
+from datetime import datetime
 
-echo 'test';
+# 現在の日時を記録
+now = datetime.now()
+print(f'Task executed at: {now}')
+
+# ファイルに記録（オプション）
+with open('log.txt', 'a') as f:
+    f.write(f'{now}\n')
